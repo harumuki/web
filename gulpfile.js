0be7b11d92956e671c2dce0074b8d4cbef8291c6
Duncan Davidson <duncandavidson@me.com>
2019-10-20 19:05:42 +0200

    Moving config into gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
+var postcssimport = require('postcss-import');
+var presetenv = require('postcss-preset-env');
 var shell = require('gulp-shell');
-const { series, parallel } = require('gulp');
+var cssnano = require('cssnano');
 
 gulp.task('css', function () {
-  return gulp.src('./style/main.css')
-    .pipe(postcss())
+  let plugins = [
+    postcssimport,
+    presetenv({ browsers: ['last 2 versions'], stage: 0, }),
+    cssnano
+  ];
+  return gulp.src('./style/masin.css')
+    .pipe(postcss(plugins))
     .pipe(gulp.dest('./public_html'));
 });
 
