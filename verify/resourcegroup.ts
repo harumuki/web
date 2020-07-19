d45ce4d53814cfdbaa79b136670a5f8dc5c7938d
Duncan Davidson <duncandavidson@me.com>
2020-07-19 21:44:38 +0200

    Test live resource group

---
+++ verify/resourcegroup.ts
@@ -0,0 +1,31 @@
+require('should')
+
+describe("resource group duncandavidson.com", () => {
+  var group = {}
+
+  before(function (done) {
+    const { exec } = require('child_process')
+    exec('az group show -n duncandavidson.com', (err, stdout, stderr) => {
+      if (!err) {
+        group = JSON.parse(stdout)
+      }
+
+      done()
+    })
+  })
+
+  it("exists", function (done) {
+    group['id'].should.exist
+    done()
+  })
+
+  it("located in westeurope", function (done) {
+    group['location'].should.equal("westeurope")
+    done()
+  })
+
+  it("successifully provisioned", function (done) {
+    group['properties']['provisioningState'].should.equal("Succeeded")
+    done()
+  })
+})
